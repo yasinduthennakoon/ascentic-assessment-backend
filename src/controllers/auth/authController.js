18dab8aa4b519afc14a251cf1982f3b0ff0b5b13
Yasindu Thennakoon <tyasindu@gmail.com>
2021-01-29 19:55:07 +0530

    feat(route): add sign in route and complete basic functions

--- src/controllers/auth/authController.js
+++ src/controllers/auth/authController.js
@@ -1,23 +1,39 @@
+/* eslint-disable no-underscore-dangle */
+const bcrypt = require('bcrypt');
 const User = require('../../model/User');
 
 exports.signup = async (req, res) => {
-
     // checking if the user already in the database
-    const emailExist =  await User.findOne({email: req.body.email})
-    if(emailExist) return res.status(400).send("email already exist");
+    const emailExist = await User.findOne({ email: req.body.email });
+    if (emailExist) return res.status(400).send('email already exist');
 
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
 
     const user = new User({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
-        password: req.body.password,
-    }); 
+        password: hashPassword,
+    });
 
     try {
-        const savedUser = await user.save();
-        res.send(savedUser);
+        await user.save();
+        return res.send({ userId: user._id, firstName: user.firstName, lastName: user.lastName, email: user.email });
     } catch (error) {
-        res.status(400).send(error);
+        return res.status(400).send(error);
     }
+};
+
+exports.signin = async (req, res) => {
+    // checking if the email exist
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send('user not in the database');
+
+    // compare password
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send('Invalid password');
+
+    return res.status(200).send('Logged In');
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth/authController');
 
-router.post('/register', authController.signup);
+router.post('/signup', authController.signup);
+router.post('/signin', authController.signin);
 
 module.exports = router;
