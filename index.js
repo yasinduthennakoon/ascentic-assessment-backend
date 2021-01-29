d9990d90bc31bfd759f9eaf5f9b40b39b3f05a44
Yasindu Thennakoon <tyasindu@gmail.com>
2021-01-29 13:13:06 +0530

    feat(index): create express server

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config({
+    path: './config.env',
+});
 
 const app = express();
 
-app.listen(3000, () => console.log('Server up adn running'));
+process.on('uncaughtException', (err) => {
+    console.log('UNCAUGHT EXCEPTION!!! shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
+// Start the server
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log(`Application is running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+    console.log('UNHANDLED REJECTION!!!  shutting down ...');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
+});
