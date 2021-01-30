9b5261fab14652c9f562dc56108b0a561073e7c1
Yasindu Thennakoon <tyasindu@gmail.com>
2021-01-30 09:38:48 +0530

    feat(todo): add create todo functionality

--- src/controllers/todo/todoController.js
+++ src/controllers/todo/todoController.js
@@ -1,9 +1,23 @@
 const Todo = require('../../model/Todos');
-const User = require('../../model/User');
 
-exports.create = async (req, res) =>{
+exports.create = async (req, res) => {
     console.log(req.payload);
-    
-    res.status(200).send('Create todo');
 
-} 
+    const newTodo = new Todo({
+        title: req.body.title,
+        description: req.body.description,
+        status: req.body.status,
+        userId: req.payload.userId,
+    });
+
+    try {
+        const savedTodo = await newTodo.save();
+        return res.status(200).send(savedTodo);
+    } catch (error) {
+        return res.status(400).send('Internal error');
+    }
+};
+
+exports.update = async (req, res) => {
+    res.status(200).send("update");
+};
