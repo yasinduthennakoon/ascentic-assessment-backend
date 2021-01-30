f80893585080038ec300ba7e1a433b382072ccf0
Yasindu Thennakoon <tyasindu@gmail.com>
2021-01-30 06:52:40 +0530

    feat(model): create todo db model

---
+++ src/model/Todos.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const todoSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255,
+    },
+    description: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 2500,
+    },
+    status: {
+        type: Boolean,
+        required: true,
+    },
+    userId: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('Todos', todoSchema);
