1720b07f47b821b4888b07ad3ce8fa9501f43bec
Yasindu Thennakoon <tyasindu@gmail.com>
2021-02-01 03:38:23 +0530

    feat(doc): create swagger api documentation for todo api calls

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 // Import Routes
 const authRoute = require('./routes/userRoutes');
 const todoRoute = require('./routes/todoRoutes');
 
 const app = express();
+
+// swagger documentation
+const swaggerOptions = {
+    definition: {
+        info: {
+            title: 'Todo Application API',
+            version: '1.0.0',
+            description: 'Todo Api for todo application management',
+            servers: ['http://localhost:5000/api'],
+        },
+    },
+    apis: ['./src/routes/*.js'],
+};
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 // Allow Cross-Origin requests
 app.use(cors());
